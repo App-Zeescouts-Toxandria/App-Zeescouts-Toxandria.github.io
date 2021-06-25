259243729758bea767bc5fdf6d821a17f4460668
App-Zeescouts-Toxandria <86495468+App-Zeescouts-Toxandria@users.noreply.github.com>
2021-06-25 23:37:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vRiBnxiSIY6Tit9vO10d6mlVp8hmrOF06lmZjpSQvx-BLTzZ3468fpAHRA4zlQhpcuOH43v5rEfKmWF/pubhtml',
-callback: function(data, tabletop) {
-console.log(data)
-},
-simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
-
+// function init() {
+// Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vRiBnxiSIY6Tit9vO10d6mlVp8hmrOF06lmZjpSQvx-BLTzZ3468fpAHRA4zlQhpcuOH43v5rEfKmWF/pubhtml',
+// callback: function(data, tabletop) {
+// console.log(data)
+// },
+// simpleSheet: true } )
+// }
+// window.addEventListener(‘DOMContentLoaded’, init)
 
 function test() {
   console.log('hello world');
